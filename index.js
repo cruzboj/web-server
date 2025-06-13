c18a4893bc7025af3d7cdc02295cad6f25b66b6e
daniel <168971595+cruzboj@users.noreply.github.com>
2025-06-13 11:52:36 +0300

    add get send test

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
 app.listen(PORT, () => {
     console.log(`Server listen to http://localhost:${PORT}`);
 })
