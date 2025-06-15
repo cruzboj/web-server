f4ed92413a859effa305c2faba25b57efabafc74
daniel <168971595+cruzboj@users.noreply.github.com>
2025-06-15 20:09:10 +0300

    seting up api && adding data/..json cards

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 8080;
-
 app.use(cors());
+
+/*
+
+*/
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
