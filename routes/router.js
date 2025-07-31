25da3e19dafbb3717e298affb202679ce2c29d55
yoav74 <yoav74@gmail.com>
2025-07-31 23:47:11 +0300

    removed comment

--- routes/router.js
+++ routes/router.js
@@ -10,44 +10,44 @@ const parser = require("../Cloudinary.js");
 
 
 //News Controller
-router.get("/news", newsController.getNews);//V
-router.post("/news",JWT.authenticateAdmin,parser.single("image"),newsController.postNews);//V
-router.delete("/news/:id",JWT.authenticateAdmin,newsController.deleteNews);//V
+router.get("/news", newsController.getNews);
+router.post("/news",JWT.authenticateAdmin,parser.single("image"),newsController.postNews);
+router.delete("/news/:id",JWT.authenticateAdmin,newsController.deleteNews);
 
 //DB controller
-router.post("/db", dbController.Register);//V
-router.post("/login", dbController.Login);//V
-router.get("/admin/db", JWT.authenticateAdmin,dbController.getAllUsers);//V
-router.get("/admin/packs", dbController.getAllPacks);//V
-router.get("/admin/cards/:packid", dbController.getCardsFromPack);//V
-router.post("/admin/createCard", JWT.authenticateAdmin, dbController.createCard);//V
-router.post("/admin/createPack",JWT.authenticateAdmin, dbController.createPack);//V
-router.delete("/admin/cards/:cardid", JWT.authenticateAdmin, dbController.deleteCard);//V
-router.delete("/admin/packs/:packid", JWT.authenticateAdmin,dbController.deletePack);//V
-router.get("/user/search/:username",dbController.searchForUser);//v
-router.get("/user/searchID/:userid", JWT.authenticateToken,dbController.getUserFromID)//v
-router.get("/cards/search/:cardid",JWT.authenticateToken,dbController.getCardFromID);//v
-router.get("/user",JWT.authenticateToken ,dbController.getUserInfo);//v
-router.get("/user/cards/:userid",JWT.authenticateToken,dbController.getCardsFromUser);//v
-router.patch("/user/cards/remove",JWT.authenticateAdmin,dbController.removeCardFromUser);//v
-router.get("/cards",dbController.getAllCards);//v
-router.patch("/user",JWT.authenticateToken,dbController.updateUser);//v
+router.post("/db", dbController.Register);
+router.post("/login", dbController.Login);
+router.get("/admin/db", JWT.authenticateAdmin,dbController.getAllUsers);
+router.get("/admin/packs", dbController.getAllPacks);
+router.get("/admin/cards/:packid", dbController.getCardsFromPack);
+router.post("/admin/createCard", JWT.authenticateAdmin, dbController.createCard);
+router.post("/admin/createPack",JWT.authenticateAdmin, dbController.createPack);
+router.delete("/admin/cards/:cardid", JWT.authenticateAdmin, dbController.deleteCard);
+router.delete("/admin/packs/:packid", JWT.authenticateAdmin,dbController.deletePack);
+router.get("/user/search/:username",dbController.searchForUser);
+router.get("/user/searchID/:userid", JWT.authenticateToken,dbController.getUserFromID)
+router.get("/cards/search/:cardid",JWT.authenticateToken,dbController.getCardFromID);
+router.get("/user",JWT.authenticateToken ,dbController.getUserInfo);
+router.get("/user/cards/:userid",JWT.authenticateToken,dbController.getCardsFromUser);
+router.patch("/user/cards/remove",JWT.authenticateAdmin,dbController.removeCardFromUser);
+router.get("/cards",dbController.getAllCards);
+router.patch("/user",JWT.authenticateToken,dbController.updateUser);
 
 //Pack Controller
-router.get("/user/packs",packController.getAvailablePacks);//v
-router.get("/pack/getPack/:packid",JWT.authenticateToken, packController.getCardFromPack);//X---------------
-router.post("/pack/insertCard",JWT.authenticateAdmin,packController.insertCard);//v
+router.get("/user/packs",packController.getAvailablePacks);
+router.get("/pack/getPack/:packid",JWT.authenticateToken, packController.getCardFromPack);
+router.post("/pack/insertCard",JWT.authenticateAdmin,packController.insertCard);
 
 //Admin Controller
-router.get("/adminTickets", JWT.authenticateAdmin, adminController.getTickets);//v
-router.post("/adminTickets", JWT.authenticateToken, adminController.postTicket);//v
-router.get("/adminTickets/:ticketid",JWT.authenticateAdmin,adminController.getTicketRequest);//v
+router.get("/adminTickets", JWT.authenticateAdmin, adminController.getTickets);
+router.post("/adminTickets", JWT.authenticateToken, adminController.postTicket);
+router.get("/adminTickets/:ticketid",JWT.authenticateAdmin,adminController.getTicketRequest);
 router.get("/adminTickets/getUser/:userid",JWT.authenticateToken ,adminController.getUserTickets);
-router.patch("/adminTickets",JWT.authenticateAdmin,adminController.updateTicket);//v
+router.patch("/adminTickets",JWT.authenticateAdmin,adminController.updateTicket);
 
 //Trade Controller
-router.post("/trade/create", JWT.authenticateToken,tradeController.sendTrade);//v
-router.post("/trade/accept",JWT.authenticateToken,tradeController.acceptTrade);//v
+router.post("/trade/create", JWT.authenticateToken,tradeController.sendTrade);
+router.post("/trade/accept",JWT.authenticateToken,tradeController.acceptTrade);
 
 
 module.exports = router;
